6084e0572a22f5feaac9698c4eae098e4eed0ed6
Eshan Hembrom <eshanjan3@gmail.com>
2021-06-01 11:28:07 +0530

    add solution to estimate pi

--- estimate.py
+++ estimate.py
@@ -1,5 +1,24 @@
 import math
+import random
 import unittest
+
+def wallis(n):
+    res=1
+    for i in range(1,n+1):
+        res*=(4*i*i)/(4*i**2-1)
+    return res*2
+
+def monte_carlo(totalDarts):
+    inCircle=0 
+
+    for i in range(1,totalDarts+1):
+        x=random.random()
+        y=random.random()
+        
+        if((x**2+y**2) <1.0):
+            inCircle+=1
+    pi=4*inCircle/totalDarts
+    return pi 
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
@@ -15,8 +34,8 @@ class TestWallis(unittest.TestCase):
 
 class TestMC(unittest.TestCase):
     def test_randomness(self):
-        pi0 = monte_carlo(15000)
-        pi1 = monte_carlo(15000)
+        pi0 = monte_carlo(1500000)
+        pi1 = monte_carlo(1500000)
         
         self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
@@ -26,7 +45,6 @@ class TestMC(unittest.TestCase):
         for i in range(500, 600):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
     
 if __name__ == "__main__":
     unittest.main()
